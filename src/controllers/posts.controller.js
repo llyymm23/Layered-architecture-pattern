15ef23cbe16c96ee99fbb1d4e3714de8000e8ca7
llyymm23 <jsy98bam1023@naver.com>
2024-02-18 23:48:18 +0900

    컨트롤러

---
+++ src/controllers/posts.controller.js
@@ -0,0 +1,32 @@
+import { PostsSerive } from '../services/posts.service.js'
+
+export class PostsController {
+    postsService = new PostsSerive();
+
+    getPosts = async (req, res, next) => {
+        try {
+            const posts = await this.postsService.findAllPosts();
+
+            return res.status(200).json({ data: posts });
+
+        } catch (err) {
+            next(err);
+        }
+
+    }
+
+    createPost = async (req, res, next) => {
+        try {
+            const { nickname, password, title, content } = req.body;
+
+            const createdPost = await this.postsService.createPost(
+                nickname, password, title, content
+            );
+
+            return res.status(201).json({ data: createdPost });
+
+        } catch (err) {
+            next(err);
+        }
+    }
+}
