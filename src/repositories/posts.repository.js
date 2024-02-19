31873966c861ad22de39a964da192dd1a29f5465
llyymm23 <jsy98bam1023@naver.com>
2024-02-19 10:31:29 +0900

    저장소 추가

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -5,7 +5,7 @@ export class PostsRepository {
         const posts = await prisma.posts.findMany();
 
         return posts;
-    }
+    };
 
     createPost = async (nickname, password, title, content) => {
         const createdPost = await prisma.posts.create({
@@ -15,5 +15,39 @@ export class PostsRepository {
         });
 
         return createdPost;
+    };
+
+    findPostById = async (postId) => {
+        const post = await prisma.posts.findUnique({
+            where: { postId: +postId },
+        })
+
+        return post;
+    };
+
+    updatePost = async (postId, password, title, content) => {
+        const updatedPost = await prisma.posts.update({
+            where: {
+                postId: +postId,
+                password: password,
+            },
+            data: {
+                title,
+                content,
+            },
+        });
+
+        return updatedPost;
+    };
+
+    deletePost = async (postId, password) => {
+        const deletedPost = await prisma.posts.delete({
+            where: {
+                postId: +postId,
+                password: password,
+            },
+        });
+
+        return deletedPost;
     };
 }
